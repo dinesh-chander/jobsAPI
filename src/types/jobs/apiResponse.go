c159823cf145270f4475e69c7629c89c2e6a6e33
dinesh.chander_wingify <dinesh.chander@wingify.com>
2017-03-28 12:28:53 +0530

    added Id in api response

--- src/types/jobs/apiResponse.go
+++ src/types/jobs/apiResponse.go
@@ -1,6 +1,7 @@
 package jobs
 
 type APIResponse struct {
+	Id      uint
 	Title   string
 	City    string
 	Country string
@@ -23,6 +24,7 @@ func ConvertToResponse(jobsList []Job, resultCount int) (response *APIResponseLi
 	for _, newJob := range jobsList {
 		var newResponseItem APIResponse
 
+		newResponseItem.Id = newJob.ID
 		newResponseItem.Apply = newJob.Apply
 		newResponseItem.Title = newJob.Title
 		newResponseItem.Country = newJob.Country
